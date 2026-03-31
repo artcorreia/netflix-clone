e4b820af2d0efae2db1200d08b4f6fce96f0443d
artcorreia <arthur.cdelila@gmail.com>
2026-03-30 23:16:53 -0300

    correção em data/profiles, imagens dos perfis aparecendo

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -1,22 +1,11 @@
 import { type Profile } from '../types/profile'
 
+import perfil1 from '../assets/perfil1.png'
+import perfil2 from '../assets/perfil2.png'
+import perfil4 from '../assets/perfil4.png'
+
 export const profiles: Profile[] = [
-  {
-    id: '1',
-    name: 'Arthur',
-    avatar: '/assets/perfil1.png',
-    alt: 'Perfil de Arthur',
-  },
-  {
-    id: '2',
-    name: 'Gabriel',
-    avatar: '/assets/perfil2.png',
-    alt: 'Perfil de Gabriel',
-  },
-  {
-    id: '3',
-    name: 'José',
-    avatar: '/assets/perfil4.png',
-    alt: 'Perfil de José',
-  },
+  { id: '1', name: 'Arthur',  avatar: perfil1, alt: 'Perfil de Arthur'  },
+  { id: '2', name: 'Gabriel', avatar: perfil2, alt: 'Perfil de Gabriel' },
+  { id: '3', name: 'José',    avatar: perfil4, alt: 'Perfil de José'    },
 ]
